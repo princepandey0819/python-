d961d4bb91a753f73efb72ceb33fdcb6f70344eb
princepandey0819 <106368897+princepandey0819@users.noreply.github.com>
2022-05-30 12:47:51 +0530

    Create fun factorial.py

---
+++ dictionary/fun factorial.py
@@ -0,0 +1,9 @@
+ (7 sloc)  181 Bytes
+   
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+n=int(input("Input a number to compute the factiorial : "))
+print(factorial(n))
