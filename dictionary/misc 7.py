9ec552467ab85a859044b7816af71f3f652096a6
princepandey0819 <106368897+princepandey0819@users.noreply.github.com>
2022-05-30 13:34:13 +0530

    Create misc 7.py

---
+++ dictionary/misc 7.py
@@ -0,0 +1,11 @@
+
+def program10():
+    text = input("Enter text to append in the file:")
+    with open("merge.txt","a") as f1:
+        f1.write(text)
+program10()
+def program10():
+    text = input("Enter text to append in the file:")
+    with open("merge.txt","a") as f1:
+        f1.write(text)
+program10()
