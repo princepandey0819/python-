fe3f32d92825028cc39f1a9277d814e2da71d811
princepandey0819 <106368897+princepandey0819@users.noreply.github.com>
2022-05-30 13:42:51 +0530

    Create numpy matrixmulti.py

---
+++ dictionary/numpy matrixmulti.py
@@ -0,0 +1,12 @@
+import numpy as np
+a = np.array([[1,2,3],
+              [4,5,6],
+              [7,8,9]])
+
+b = np.array([[2,3,4],
+              [5,6,7],
+              [8,9,10]])
+
+o = a*b
+
+print(o)
